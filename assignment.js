b3240e6b8cdfff6e83c631d392463295b663a08e
hoaiduke <105718496+hoaiduke@users.noreply.github.com>
2024-04-19 16:38:29 +0200

    assignment.js

---
+++ assignment.js
@@ -0,0 +1,58 @@
+// Coding Challenge #3:
+/*
+const firstScoreDolphins = 96;
+const secondScoreDolphins = 108;
+const thirdScoreDolphins = 89;
+
+const firstScoreKoalas = 88;
+const secondScoreKoalas = 91;
+const thirdScoreKoalas = 110;
+*/
+
+// Bonus 1:
+/*
+const firstScoreDolphins = 97;
+const secondScoreDolphins = 112;
+const thirdScoreDolphins = 101;
+
+const firstScoreKoalas = 109;
+const secondScoreKoalas = 95;
+const thirdScoreKoalas = 123;
+*/
+
+// Bonus 2:
+/*
+const firstScoreDolphins = 97;
+const secondScoreDolphins = 112;
+const thirdScoreDolphins = 101;
+
+const firstScoreKoalas = 109;
+const secondScoreKoalas = 95;
+const thirdScoreKoalas = 106;
+*/
+/*
+const scoreDolphins = (firstScoreDolphins + secondScoreDolphins + thirdScoreDolphins) / 3;
+const scoreKoalas = (firstScoreKoalas + secondScoreKoalas + thirdScoreKoalas) / 3;
+
+console.log(scoreDolphins, scoreKoalas);
+*/
+/*
+if (scoreDolphins > scoreKoalas) {
+    console.log(`Dolphins win the trophy`)
+} else if (scoreKoalas > scoreDolphins) {
+    console.log(`Koalas win the trophy`)
+} else if (scoreDolphins === scoreKoalas) {
+    console.log(`Both win the trophy`)
+}
+*/
+/*
+if (scoreDolphins >= 100 && scoreKoalas >= 100 && scoreDolphins > scoreKoalas) {
+    console.log(`Dolphins win the trophy`);
+} else if (scoreDolphins >= 100 && scoreKoalas >= 100 && scoreDolphins < scoreKoalas) {
+    console.log(`Koalas win the trophy`);
+} else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
+    console.log(`Both win the trophy`);
+} else {
+    console.log(`No one win the trophy`);
+}
+*/
